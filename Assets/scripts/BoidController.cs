1a7a87f241dda8639a13bc1c2417cabafe5336af
Lloyd Phillips <Driven.lloyd@gmail.com>
2018-02-12 14:38:07 +0000

    Initial Commit

---
+++ Assets/scripts/BoidController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour
+{
+
+    //create global controls here
+    public GameObject m_boidPrefab;
+
+    public LayerMask m_searchLayer;
+
+    public int m_spawnCount = 10;
+
+    public float m_spawnRadius = 4.0f;
+
+    [Range(5.0f, 60.0f)]
+    public float m_destinatiinVariation = 6.0f;
+
+    [Range(0.1f, 20.0f)]
+    public float m_velocity = 6.0f;
+
+    [Range(4.0f, 20.0f)]
+    public float m_neighbourDistance = 4.0f;
+
+    [Range(1.0f, 2.0f)]
+    public float m_velocityVariation = 1.2f;
+
+    [Range(0.001f, 2.0f)]
+    public float m_rotationDiffusion = 10.0f;
+
+    public float m_controllerVelocity = 5.0f;
+
+    Vector3 mouse = new Vector3(0.0f, 0.0f, 0.0f);
+    List<Boids> myBoids = new List<Boids>();
+
+    void Start()
+    {
+        //spawn boids here.
+        for (int i = 0; i < m_spawnCount; i++)
+        {
+            Spawn();
+        }
+         
+
+    }
+
+    void Update()
+    {
+        Debug.Log(myBoids.Count);
+        m_controllerVelocity = 0.0f;
+        foreach (Boids b in myBoids)
+        {
+            m_controllerVelocity += b.Velocity;
+        }
+
+       
+        m_controllerVelocity /= myBoids.Count;
+
+
+        //COME BACK TO THIS - Trying to get a \/\/\/\ movement along the foward vector. 
+        /*
+        float wave = Mathf.Sin(Time.time);
+        Vector3 rotation2 = new Vector3(0, wave * Time.deltaTime , 0);
+        transform.Rotate(rotation2);
+        */
+
+      
+
+        if (Input.GetMouseButton(0))
+        {
+            //Cast a ray
+            RaycastHit move;
+            //between camera (tagged as main) and where the mouse clicks (best to put a "Floor" down)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out move))
+            {
+                //get the point of where the mouse hits.
+                mouse = move.point;
+                //set the direction to be where we want to go near the mouse.
+            }
+        }
+        Vector3 newDirection = (mouse - transform.position);
+        newDirection = newDirection.normalized;
+         Quaternion newRotation = Quaternion.FromToRotation(transform.forward, newDirection);
+         if (transform.rotation != newRotation)
+         {
+             transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, 0.3f);
+         }
+
+
+        //Constantly move forward for now. 
+        //transform.position += (transform.forward * (m_controllerVelocity * Time.deltaTime));
+        transform.position += (newDirection * (m_controllerVelocity * Time.deltaTime));
+    }
+
+
+    public GameObject Spawn()
+
+    {
+        return Spawn(Random.insideUnitSphere * m_spawnRadius);
+
+    }
+
+
+
+    public GameObject Spawn(Vector3 position)
+
+    {
+
+        Quaternion rotation = new Quaternion(0, 0, 0,0); // Quaternion.Slerp(transform.rotation, Random.rotation, m_rotationDiffusion);
+
+        GameObject boid = Instantiate(m_boidPrefab, position, rotation) as GameObject;
+
+        boid.GetComponent<Boids>().controller = this;
+        myBoids.Add(boid.GetComponent<Boids>());
+
+
+        return boid;
+
+    }
+}
---
+++ Assets/scripts/Boids.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boids : MonoBehaviour
+{
+    public BoidController controller;
+    public float animationSpeedVariation = 0.2f;
+    int identifier;
+
+
+    Vector3 mouse = new Vector3(0.0f, 0.0f, 0.0f);
+    float randomDestination;
+
+    // Use this for initialization
+
+   
+
+    float noiseOffset;
+    private float velocity;
+    public float Velocity
+    {
+        get
+        {
+            return velocity;
+        }
+
+        set
+        {
+            velocity = value;
+        }
+    }
+
+    void Start()
+    {
+        noiseOffset = Random.value * 10.0f;
+        var animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            animator.speed = Random.Range(-1.0f, 1.0f) * animationSpeedVariation + 1.0f;
+        }
+        randomDestination = Random.Range(3.0f, 3.0f + controller.m_destinatiinVariation);
+        identifier = GetInstanceID();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //create local variables to get transform inforation. 
+        Vector3 currentPosition = transform.position;
+        Quaternion currentRotation = transform.rotation;
+
+        // Initializes the 3 vectors for the 3 reynolds rules of boid systems
+        Vector3 separation = Vector3.zero;
+        Vector3 alignment = controller.transform.forward;
+        Vector3 cohesion = controller.transform.position;
+               
+
+        //get some random noise. 
+        float noise = Mathf.PerlinNoise(Time.time, noiseOffset) * 2.0f - 1.0f;
+     
+        //Slowly rotate towards where the mouse points to (No snap)
+
+        //Get an array of all the nearby boids.
+        Collider[] nearbyBoids = Physics.OverlapSphere(currentPosition, controller.m_neighbourDistance, controller.m_searchLayer);
+        int neighbours = nearbyBoids.Length;
+        foreach (Collider b in nearbyBoids)
+        {
+            //
+            if (b.gameObject == gameObject) continue;
+            Vector3 neighbourDirection = currentPosition - b.transform.position;
+            float neighbourDistance = neighbourDirection.magnitude;
+            float scaler = Mathf.Clamp01(1.0f - neighbourDistance / controller.m_neighbourDistance);
+            separation += (neighbourDirection * scaler / neighbourDistance);
+            //Debug.Log("separation");
+            // add all boids forward vectors together. 
+            alignment += b.transform.forward;
+            //Get all the positions and add them together
+            cohesion += b.transform.position;
+        }
+        float average = 1.0f / neighbours;
+        cohesion *= average;
+        alignment *= average;
+        cohesion = (cohesion - currentPosition).normalized;
+
+        Vector3 direction = separation + alignment + cohesion;
+
+
+        Quaternion newRotation = Quaternion.FromToRotation(Vector3.forward, direction );
+        float ip = Mathf.Exp(-controller.m_rotationDiffusion* noise*Time.deltaTime);
+        if (currentRotation != newRotation)
+        {
+            transform.rotation = Quaternion.Lerp(currentRotation, newRotation, 0.3f);
+        }
+
+       // Debug.Log(identifier + " HAS " + nearbyBoids.Length + " NEIGHBOURS");
+        /* if (Vector3.Distance(currentPosition, mouse) >= randomDestination)
+         {
+             float velocity = controller.m_velocity * (1.0f + noise * controller.m_velocityVariation);
+             //While the distance between the currentposition and where the mouse clicked
+             //If the transform isn't facing the direction turn towards the direction
+             if (transform.forward != direction)
+             {
+
+                 //Debug.Log("TURNING");
+                 Vector3 tpos = currentPosition + (direction + transform.right.normalized) * (velocity * Time.deltaTime);
+                 transform.position = tpos;
+             }
+             else
+             {
+                //    Debug.Log("FORWARD");
+                 Vector3 tpos = currentPosition + direction * (velocity * Time.deltaTime);
+                 transform.position = tpos;
+             }
+         }*/
+       
+        velocity = controller.m_velocity * (1.0f + noise * controller.m_velocityVariation);
+        transform.position  = currentPosition + transform.forward * (velocity * Time.deltaTime);
+
+
+    }
+}
